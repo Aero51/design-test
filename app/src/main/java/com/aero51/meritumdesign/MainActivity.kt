86ee2e6c58f606cb2e0e9599d683908e8f528531
nikola.srdoc <nikola.srdoc@gmail.com>
2021-09-22 22:19:51 +0200

    first commit

---
+++ app/src/main/java/com/aero51/meritumdesign/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aero51.meritumdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
